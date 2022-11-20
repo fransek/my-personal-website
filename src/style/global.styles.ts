3f1f44ee940e56c2c8b67ddd9d6311d32aea4b37
Frans Ekman <104152557+fransek@users.noreply.github.com>
2022-11-20 20:30:27 +0100

    gradient

--- src/style/global.styles.ts
+++ src/style/global.styles.ts
@@ -1,7 +1,14 @@
+import { Theme } from './theme.styles'
 import { createGlobalStyle } from 'styled-components'
+import { theme } from 'style/theme.styles'
 
 export const GlobalStyle = createGlobalStyle`
   body {
+      min-height: 100vh;
+      background-image: linear-gradient(to bottom right, ${(props: Theme) =>
+        props.theme.bg.gradient_1}, ${(props: Theme) =>
+  props.theme.bg.gradient_2});
+      color: white;
       margin: 0;
       padding: 0;
       font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
--- src/style/theme.styles.ts
+++ src/style/theme.styles.ts
@@ -1,3 +1,8 @@
-export const theme = {}
+export const theme = {
+  bg: {
+    gradient_1: '#89435c',
+    gradient_2: '#1a1846',
+  },
+}
 
 export type Theme = { theme: typeof theme }
