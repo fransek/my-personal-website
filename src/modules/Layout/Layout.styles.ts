456a622fa9bdb8ec6c9a7e368471b4023dce073b
Frans Ekman <104152557+fransek@users.noreply.github.com>
2022-12-04 14:42:39 +0100

    margin bottom fix

--- src/modules/Layout/Layout.styles.ts
+++ src/modules/Layout/Layout.styles.ts
@@ -2,6 +2,7 @@ import { media } from 'style/util.styles'
 import styled from 'styled-components'
 
 export const _Content = styled.div`
+  margin-bottom: 3rem;
   @media (min-width: ${media.tablet}) {
     width: clamp(${media.tablet}, 70%, ${media.laptop_l});
     margin: auto;
