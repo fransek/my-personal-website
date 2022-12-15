3e605c6008a951e0b82762d943758d5a95308296
Frans Ekman <104152557+fransek@users.noreply.github.com>
2022-12-15 19:38:14 +0100

    MD

--- src/components/MD/MD.tsx
+++ src/components/MD/MD.tsx
@@ -5,10 +5,17 @@ import { SpecialComponents } from 'react-markdown/lib/ast-to-react'
 
 interface IMDProps {
   children: string
+  customize?: Partial<
+    Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents
+  >
 }
 
-export const MD = ({ children }: IMDProps) => {
-  return <ReactMarkdown components={components}>{children}</ReactMarkdown>
+export const MD = ({ children, customize }: IMDProps) => {
+  return (
+    <ReactMarkdown components={{ ...components, ...customize }}>
+      {children}
+    </ReactMarkdown>
+  )
 }
 
 const components: Partial<
